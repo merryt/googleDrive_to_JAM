a316061282f504b7fe3409650c9c82cfc71242b6
Tyler Merry <tmerry@fool.com>
2019-04-07 22:27:27 -0600

    Adding .vuepress, vor the styles

---
+++ markdown/.vuepress/theme/layouts/Post.vue
@@ -0,0 +1,36 @@
+<template>
+
+  <div>
+    <div class="bio-line">
+      <Author />
+      Published on: {{date}}
+    </div>
+    <Content :custom="false"/>
+  </div>
+
+
+</template>
+
+<style>
+ .bio-line{
+     display:flex;
+     justify-content: space-between;
+ }
+
+</style>
+
+<script>
+ import Author from '../components/Author.vue'
+ export default{
+     components: {Author},
+     computed: {
+         date(){
+             const{frontmatter} = this.$page
+             return frontmatter.Date.replace(/\\/g,  "")
+         }
+
+     }
+
+ }
+
+</script>
