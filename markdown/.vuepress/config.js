ed6b3c895dcff4ecd5d1cd8be957c070b94c75be
Tyler Merry <tymerry@gmail.com>
2019-10-16 18:17:00 -0600

    fix that fat finger

--- markdown/.vuepress/config.js
+++ markdown/.vuepress/config.js
@@ -8,6 +8,6 @@ module.exports = {
         ]
     },
     head: [
-        ['meta', { name: 'google-site-verification', content: '8bkDTzGwYEt1P4NaDKuhxLUJbosgzc0Pz3tPP29wAgEI fe' }]
+        ['meta', { name: 'google-site-verification', content: '8bkDTzGwYEt1P4NaDKuhxLUJbosgzc0Pz3tPP29wAgE' }]
     ]
 }
