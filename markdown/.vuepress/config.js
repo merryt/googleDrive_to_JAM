0731ce859f4d71ed73b3153b21b313b106f3d236
Tyler Merry <tymerry@gmail.com>
2019-10-16 18:12:03 -0600

    adding webmaster tools

--- markdown/.vuepress/config.js
+++ markdown/.vuepress/config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  title: "Tyler Merry",
-  themeConfig: {
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'External', link: 'https://google.com' },
+    title: "Tyler Merry",
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ]
+    },
+    head: [
+        ['meta', { name: 'google-site-verification', content: '8bkDTzGwYEt1P4NaDKuhxLUJbosgzc0Pz3tPP29wAgEI fe' }]
     ]
-  }
 }
