34edb75f8ece69f10fc7e57d1ef3bb6a39e87d8d
tmerry <tymerry@gmail.com>
2019-11-17 14:42:11 -0700

    invalid link, my fault

--- generate_site_map.js
+++ generate_site_map.js
@@ -28,7 +28,7 @@ retreiveFileNamesFromFolder = function(targetFolder, callback){
 
 convertFileExtensionToHTML = function(fileName){
     HMTLString = fileName.toString().replace(/\..*/gim, ".html")
-    return 'https"//tymerry.com/' + HMTLString
+    return 'https://tymerry.com/' + HMTLString
 }
 
 saveSitemap = function(arrayOfHTMLFiles){
